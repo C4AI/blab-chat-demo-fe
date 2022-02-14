c51849b96f8edc8f33376e9c4b7240b6a0e4814f
Vinícius <matosvb@gmail.com>
2022-02-14 16:03:05 -0300

    Move translations to separate files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { Container, createTheme, ThemeProvider } from "@mui/material";
+import { Button, Container, createTheme, ThemeProvider } from "@mui/material";
 import { Suspense, useEffect, useState } from "react";
 
 import "./App.css";
 import Lobby from "./Lobby/Lobby";
 import Chat from "./Chat/Chat";
 import axios from "axios";
+import { useTranslation } from "react-i18next";
 
 const theme = createTheme({
   palette: {
@@ -13,7 +14,9 @@ const theme = createTheme({
   },
 });
 
-function App() {
+function Main() {
+  const { i18n } = useTranslation();
+
   const conversationIdKey = "conversationId";
 
   const [conversationId, setConversationId] = useState(
@@ -45,41 +48,52 @@ function App() {
   }
 
   return (
-    <Suspense fallback="...">
+    <Container component="main" maxWidth="xs">
+      {process.env.REACT_APP_CHAT_MODE === "rooms" ? (
+        !conversationId ? (
+          <Lobby
+            handleRoomCreation={createConversation}
+            handleJoining={joinConversation}
+          />
+        ) : (
+          <Chat
+            conversationId={conversationId}
+            onLeave={() => setConversationId(null)}
+          />
+        )
+      ) : (
+        <p>
+          INVALID MODE. PLEASE SET THE ENVIRONMENT VALUE{" "}
+          <code>REACT_APP_CHAT_MODE</code> TO <i>rooms</i> OR <i>bots</i> AND
+          TRY AGAIN.
+        </p>
+      )}
+      {process.env.NODE_ENV === "development" ? (
+        <div>
+          <p>
+            ENVIRONMENT: <b>{process.env.NODE_ENV}</b>
+          </p>
+          <p>
+            MODE: <b>{process.env.REACT_APP_CHAT_MODE}</b>
+          </p>
+          <p>
+            LANGUAGE: <b>{i18n.language}</b> (
+            {i18n.languages ? i18n.languages.join(", ") : "?"}) &rarr;{" "}
+            {i18n.resolvedLanguage}
+          </p>
+        </div>
+      ) : (
+        ""
+      )}
+    </Container>
+  );
+}
+
+function App() {
+  return (
+    <Suspense fallback={<div>Loading... </div>}>
       <ThemeProvider theme={theme}>
-        <Container component="main" maxWidth="xs">
-          {process.env.REACT_APP_CHAT_MODE === "rooms" ? (
-            !conversationId ? (
-              <Lobby
-                handleRoomCreation={createConversation}
-                handleJoining={joinConversation}
-              />
-            ) : (
-              <Chat
-                conversationId={conversationId}
-                onLeave={() => setConversationId(null)}
-              />
-            )
-          ) : (
-            <p>
-              INVALID MODE. PLEASE SET THE ENVIRONMENT VALUE{" "}
-              <code>REACT_APP_CHAT_MODE</code> TO <i>rooms</i> OR <i>bots</i>{" "}
-              AND TRY AGAIN.
-            </p>
-          )}
-          {process.env.NODE_ENV !== "production" ? (
-            <div>
-              <p>
-                ENVIRONMENT: <b>{process.env.NODE_ENV}</b>
-              </p>
-              <p>
-                MODE: <b>{process.env.REACT_APP_CHAT_MODE}</b>
-              </p>
-            </div>
-          ) : (
-            ""
-          )}
-        </Container>
+        <Main />
       </ThemeProvider>
     </Suspense>
   );
--- src/i18n.js
+++ src/i18n.js
@@ -1,46 +1,11 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-// TODO: move translations to separate JSON Files
-const resources = {
-  en: {
-    translation: {
-      newConversation: "New:",
-      newConversationName: "Conversation name",
-      yourName: "Name",
-      joinConversation: "Join conversation",
-      createConversation: "Create conversation",
-      createdConversation: "Conversation created",
-      joinedConversation: "<0>{{name}}</0> joined the conversation",
-      youJoinedConversation: "You joined the conversation",
-      leftConversation: "<0>{{name}}</0> left the conversation",
-      youLeftConversation: "You left the conversation",
-      typeMessage: "Type a message",
-      leaveConversation: "Leave",
-    },
-  },
-  "pt-BR": {
-    translation: {
-      newConversation: "Nova:",
-      newConversationName: "Nome da conversa",
-      yourName: "Nome",
-      joinConversation: "Entrar na conversa",
-      createConversation: "Criar conversa",
-      createdConversation: "Conversa criada",
-      joinedConversation: "<0>{{name}}</0> entrou na conversa",
-      youJoinedConversation: "Você entrou na conversa",
-      leftConversation: "<0>{{name}}</0> saiu da conversa",
-      youLeftConversation: "Você saiu da conversa",
-      typeMessage: "Digite uma mensagem",
-      leaveConversation: "Sair",
-    },
-  },
-};
-
 i18n
+  .use(Backend)
   .use(LanguageDetector)
-
   .use(initReactI18next)
   .init({
     debug: true,
@@ -48,7 +13,6 @@ i18n
     interpolation: {
       escapeValue: false,
     },
-    resources,
   });
 
 export default i18n;

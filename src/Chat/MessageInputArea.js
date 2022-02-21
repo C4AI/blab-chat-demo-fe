bdc5d7782302662d6be0db4b4f4250514ef61aa5
Vinícius <matosvb@gmail.com>
2022-02-21 14:28:20 -0300

    Add documentation to MessageInputArea

--- src/Chat/MessageInputArea.js
+++ src/Chat/MessageInputArea.js
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import { v4 as uuidv4 } from "uuid";
 import SendIcon from "@mui/icons-material/Send";
 import { forwardRef, useImperativeHandle, useRef, useState } from "react";
@@ -5,19 +6,33 @@ import { IconButton, InputAdornment, TextField } from "@mui/material";
 import { Trans } from "react-i18next";
 import { Message, MessageTypes } from "./data-structures";
 
+/**
+ * Display elements where the user can insert a message and send it.
+ *
+ * @type React.FC<MessageInputAreaPropTypes>
+ */
 const MessageInputArea = forwardRef(function ({ onSendMessage }, ref) {
   const textFieldRef = useRef(null);
   useImperativeHandle(ref, () => ({
+    /** Focus the text field */
     focus() {
       textFieldRef.current.focus();
     },
   }));
 
-  const [typedMessage, setTypedMessage] = useState("");
+  // text typed by the user
+  const [typedText, setTypedText] = useState("");
 
+  /** If there is a message (typed, attachment inserted, etc.),
+   * store its data in a {@link Message} instance; otherwise,
+   * return `null`.
+   *
+   * @return a {@link Message} with the data the user inserted,
+   * or `null` if the fields are blank
+   */
   function collectMessage() {
-    const text = typedMessage.trim();
-    if (!text) return;
+    const text = typedText.trim();
+    if (!text) return null;
     return new Message(
       MessageTypes.TEXT,
       new Date(),
@@ -27,26 +42,32 @@ const MessageInputArea = forwardRef(function ({ onSendMessage }, ref) {
     );
   }
 
+  /**
+   * If there is a message (typed, attachment inserted, etc.),
+   * collect its data in  a {@link Message} instance, call
+   * the callback function and clear the fields.
+   * @returns
+   */
   function sendMessage() {
     const message = collectMessage();
     if (message === null) return;
-    setTypedMessage("");
+    setTypedText("");
     onSendMessage(message);
   }
 
   return (
     <TextField
       inputRef={textFieldRef}
-      value={typedMessage}
+      value={typedText}
       fullWidth
       multiline
       minRows={4}
       label={<Trans i18nKey="typeMessage">Type a message</Trans>}
       variant="outlined"
-      onChange={(e) => setTypedMessage(e.target.value)}
+      onChange={(e) => setTypedText(e.target.value)}
       onKeyPress={(e) => {
         if (e.key === "Enter" && !e.shiftKey) {
-          sendMessage(typedMessage.trim());
+          sendMessage(typedText.trim());
           e.preventDefault();
         }
       }}
@@ -54,9 +75,9 @@ const MessageInputArea = forwardRef(function ({ onSendMessage }, ref) {
         endAdornment: (
           <InputAdornment position="end">
             <IconButton
-              disabled={!typedMessage.trim()}
+              disabled={!typedText.trim()}
               onClick={(e) => {
-                sendMessage(typedMessage.trim());
+                sendMessage(typedText.trim());
               }}
               onMouseDown={(e) => e.preventDefault()} // don't lose focus
             >
@@ -68,5 +89,11 @@ const MessageInputArea = forwardRef(function ({ onSendMessage }, ref) {
     />
   );
 });
+
+const MessageInputAreaPropTypes = {
+  /** called when a message is sent */
+  onSendMessage: PropTypes.func.isRequired,
+};
+MessageInputArea.propTypes = MessageInputAreaPropTypes;
 
 export default MessageInputArea;
